f90eb7cc828a7172982da23c1ed1f2a1798a9d52
David4145 <david.mathieu4569@gmail.com>
2020-03-05 15:05:36 +0100

    try to make list

--- src/containers/DataContainer.jsx
+++ src/containers/DataContainer.jsx
@@ -1,9 +1,32 @@
 import React, { Component } from 'react';
-import  Axios  from 'axios';
-
-
-
+import Axios from 'axios';
+import { ListGroup } from 'react-bootstrap';
 
 export default class DataContainer extends Component {
-  
+  state = {
+    data: null
+  }
+  componentDidMount = () => {
+    const url = `https://randomuser.me/api/?page=1&results=10&seeda4accc8af17f6b1c&nat=fr`;
+
+    Axios.get(url)
+      .then(response => {
+        this.setState({ data: response.data });
+        console.log(response.data);
+      })
+      .catch(error => console.log(error));
+
+      return this.response.data;
+
+  }
+
+  render = () => {
+this.componentDidMount();
+   
+    return (
+      <ListGroup>
+        {this.data.results.map()}
+      </ListGroup>
+    )
+  }
 }
